2caa7720bd941bca2a44f7efa7a3d64696f8f261
Chris Young <cy_borg5@cyborg5.com>
2017-06-18 15:29:49 -0400

    Fixed problem with G.I.Cable decoder
    Fixed problem with G.I.Cable decoder

--- source/IRLib_P09_GICabled.py
+++ source/IRLib_P09_GICabled.py
@@ -15,7 +15,7 @@ class IRdecodeGICable(IRLibDecodeBase.IRLibDecodeBase):
 		and self.MATCH(IRrecvPCI.decodeBuffer[2], 490):
 			self.bits=0
 			self.value=-1
-			self.protocolNum=IRLibDecodeBase.GICABLE
+			self.protocolNum=IRLibProtocols.GICABLE
 			return True
 		if (not self.decodeGeneric(35,8820,4410,490,4410,2205)):
 			return False
